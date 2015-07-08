582d72b2d8b9df3a4eb73c613b94dfb557bba3e4
wilton <wilton@WilsonLaptop.home>
2015-07-08 20:55:03 +0100

    Finished Writing Generic DAO

--- src/java/EJB/GenericDAO.java
+++ src/java/EJB/GenericDAO.java
@@ -25,10 +25,52 @@ public abstract class GenericDAO
         Configuration config = new Configuration().configure();
         StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(config.getProperties());
         SessionFactory sessionfactory = config.buildSessionFactory(builder.build());
-        return sessionfactory;
-        
+        return sessionfactory;   
+    }
+    
+    public Session openCurrentSession()
+    {
+        this.currentSession = getSessionFactory().openSession();
+        return this.currentSession;
+    }
+    
+    public Transaction openCurrentTransaction()
+    {
+       this.currentSession = getSessionFactory().openSession();
+       this.currentTransaction =this.currentSession.beginTransaction();
+       return this.currentTransaction;
     }
     
    
+    public void closeCurrentSession()
+    {
+        this.currentSession.close();
+    }
+    
+    public void closeCurrentTransaction()
+    {
+        this.currentSession.close();
+        this.currentTransaction.commit();
+    }
+    
+    public Session getCurrentSession()
+    {
+        return this.currentSession;
+    }
+    
+    public Transaction getCurrentTransaction()
+    {
+        return this.currentTransaction;
+    }
+    
+    public void setCurrentSession(Session session)
+    {
+        this.currentSession = session;
+    }
+    
+    public void setCurrentTransaction(Transaction transaction)
+    {
+        this.currentTransaction = transaction;
+    }
     
 }
