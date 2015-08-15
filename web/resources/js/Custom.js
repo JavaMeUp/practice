75673a73ba9c049879892c33d4d095d6dff734ed
wilton <wilton@WilsonLaptop.home>
2015-08-15 10:13:02 +0100

    Fixed Student locator and class locator. Need to move to styling the page as well as creating the web services for the ajax to call

---
+++ web/resources/js/Custom.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//This Script requires JQuery to be loaded in for it to work.
+
+var Call = function(WebServiceURL)
+{
+    var webURL = WebServiceURL; 
+    
+    this.caller= function()
+    {
+        
+        
+    };
+};
