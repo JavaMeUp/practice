502287b36d97ca93fa9fcd39a03c748f7217e2de
wilton <wilton@WilsonLaptop.home>
2015-08-23 17:15:24 +0100

    Worked on display. Have come across an issue, when user 1 logs in and logs out the cookies are created and deleted as usual however in the logging in of user2 after user1. The details of user1 is still present. Believe this is because the LoggeddInpageBean has been created with the previous user and does not generate again therefore need to instead of relying on cookies to pass on different screens going to have to place this in a managed bean. Also this needs to be used in the filter.

---
+++ src/java/ManagedBeans/UserLogedIn.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagedBeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author wilton
+ */
+@Named(value = "userLogedIn")
+@Dependent
+public class UserLogedIn {
+
+    /**
+     * Creates a new instance of UserLogedIn
+     */
+    public UserLogedIn() 
+    {
+        
+    }
+    
+}
